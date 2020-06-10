52621b1c0cd6d58b4a327032557e166aa1b53080
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-06-10 11:08:08 -0700

    Constructors and Instantiations
    Created some fields and constructors to read in the players.

--- Assassin.java
+++ Assassin.java
@@ -11,16 +11,39 @@ package finalProject;
  * Creator Notes: 
  * Have a "Graveyard" for killed players that can be accessed anytime.
  * Have a method that shows the current stats of the game.
- * If there is one person left alive then tehy are the winner and game over.
+ * If there is one person left alive then they are the winner and game over.
  * 
  */
 
 import java.util.*;
 public class Assassin 
 {
+	//Player fields
+	private static LinkedList<String> players;
+	private static LinkedList<String> grave;
+	private static int playerCount;
+	
+	//Default construction
 	public Assassin()
 	{
-		
+		  players = new LinkedList<>();
+		  grave = new LinkedList<>();
+		  playerCount = 0;
+	}
+	
+	public Assassin(Scanner temp)
+	{
+		readPlayers(temp);
+	}
+	
+	//Reads players into the game from "Players.txt"
+	public void readPlayers(Scanner temp) 
+	{
+		while(temp.hasNext()) //Add all the scanner names into the players list.
+		{
+			players.add(temp.next());
+			playerCount++;
+		}
 	}
 	
 }
