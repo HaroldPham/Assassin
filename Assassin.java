9323919744d60139b302c1a3391b3c22d11dff9b
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-06-09 17:17:51 -0700

    Starting Code Additions
    Bare bones structure

---
+++ Assassin.java
@@ -0,0 +1,26 @@
+package finalProject;
+/* Harold Pham, CS 211(B), Professor James Livingston, Final Project
+ * Creation Date: 6/9/2020
+ * Details: Chapter 16 Project #4
+ * File Description: This "Assassin" Class will hold the technical code of the game.
+ * Overview: Assassin is a game that takes a file of names, stores it into a LinkedList, 
+ * and slowly removes individual names as if they were killed. 
+ */
+
+/*
+ * Creator Notes: 
+ * Have a "Graveyard" for killed players that can be accessed anytime.
+ * Have a method that shows the current stats of the game.
+ * If there is one person left alive then tehy are the winner and game over.
+ * 
+ */
+
+import java.util.*;
+public class Assassin 
+{
+	public Assassin()
+	{
+		
+	}
+	
+}
