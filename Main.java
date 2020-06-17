972fb8ff7ad240703fb12a3f0afc6cd47f107fb9
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2020-06-17 16:56:38 -0700

    Final Main Test Method Update
    Additions:
    - Comments
    - Structure change

--- Main.java
+++ Main.java
@@ -20,8 +20,10 @@ public class Main
 	
 	
 //SYSTEM METHODS
+	
 	public static void main(String[] args) 
 	{
+		//Get the user started first with the game idea
 		System.out.println("Welcome to Assassin:");
 		System.out.println("Synopsis: Provided with a text file named \"Players\" by either player or system "
 				+ "\nthis game will run through all the participants listed until one name is left. "
@@ -29,21 +31,24 @@ public class Main
 				+ "\nother features of this game.");
 		System.out.println("\nContinue? (Press any key and enter): ");
 		strChoice = sysScan.nextLine();
-		if(strChoice != "")
+		if(strChoice != "") //Start the game sequences
 		{
 			optimize();
 			start();
 		}
 	}
 	
-	private static void optimize() //acts as if it were the constructor of the class
+	
+	//acts as if it were the constructor of the class
+	private static void optimize() 
 	{
 		//First get the players list
 		boolean ok = false;
 		Scanner temp = new Scanner(System.in); //Temporary instantiation
-		while(ok == false)
+		
+		while(ok == false) //Read the Players.txt file into the game
 		{
-		    try //Try reading the Players.txt file
+		    try
 			{
 		    	temp = new Scanner(new File("Players.txt"));
 		    	ok = true;
@@ -65,9 +70,9 @@ public class Main
 		
 		game = new Assassin(temp);
 		
-		System.out.println("Players shuffled.\n");
+		System.out.println("Players shuffled.\n"); //The Assassin constructor should shuffle the players automatically.
 		
-		while(ok == false)
+		while(ok == false) //Display the players and ask if there are more.
 		{
 			System.out.println("These are your players, would you like to add more? (Choose a number): \n\t" + game.alive());
 			System.out.println("\n1: Yes\n2: No");
@@ -93,50 +98,54 @@ public class Main
 		
 	}
 	
+	
 	//Game starts.
 	private static void start()
 	{
+		//Give the user the rules of the game
 		System.out.println("\nRules: As the rounds progress and as you make your choices please be sure to only enter your choice \nthrough whole numbers unless"
 				+ " otherwise noted. (Press any key and enter to continue):\n");	
 		strChoice = sysScan.next();
 		
 		System.out.println("Game Starting...\n");
 		
-		while(!game.lastPersonStanding())
+		while(!game.lastPersonStanding()) //The actual game mechanics start here, loop until there is one person left
 		{
 			System.out.println("Round " + game.getRound() + ":");
 			game.statHUD();
-			round();
-		}
+			round(); //This will display the game options for the user
+		} 
 		
+		//Show the winner to the client.
 		System.out.println("\nAll are gone except for one..." + game.getAlivePlayer(0) + " is the last one standing! (Press any key and enter to continue): ");
-		strChoice = sysScan.next();
-		System.out.println("\nPost game results: ");
+		strChoice = sysScan.next(); //Create a pause in the system for time to read
+		
+		System.out.println("\nPost game results: "); //Show all of stats.
 		System.out.println("\tFinal Round: " + (game.getRound()-1) + "\n");
 		game.statHUD();
 	}
 	
-	//Will facilitate each round and handle the game.
+	
+	//Will facilitate each round and handle the game. Asks for user input each round.
 	public static void round()
 	{
 		System.out.println("1: Proceed\n2: End Game\n");
 		
 		numChoice = sysScan.nextInt();
 			
-		if(numChoice == 1)
+		if(numChoice == 1) //If the user wants to continue, kill someone and move on.
 		{
 			int killZone = (int) (Math.random() * game.killRingSize()); //Generate the index of the killed person
-			System.out.println(game.kill(killZone));
+			System.out.println(game.kill(killZone)); //Kill person at that index.
 			game.nextRound();
 		}
-
-		else if(numChoice == 2)
+		else if(numChoice == 2) //If the user needs to, end the game.
 		{
 			System.out.println("Game Ending...");
 			System.exit(0);         //Taught to me by Aleks G. on StackOverflow. 
 									//https://stackoverflow.com/questions/22452930/terminating-a-java-program
-		}
-		else
+		} 
+		else //If a number outside of expectations is entered restart the round choices.
 		{
 			System.out.println("Please enter a valid number.");
 			round();
